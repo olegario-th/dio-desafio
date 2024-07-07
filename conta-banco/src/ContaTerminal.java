b61c0bf653d0e144c9f02720dfa8240228b3c3b9
olegario-th <tholegarios@gmail.com>
2024-07-07 17:51:01 -0300

    finalização do código

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       /* TO DO:
+       - CONHECER E IMPORTAR A CLASSE SCANNER;
+       - EXIBIR AS MENSAGENS PARA O NOSSO USUÁRIO
+       - OBTER PELA CLASSE SCANNER OS VALORES DIGITADOS NO TERINAL
+       - EXIBIR A MENSAGEM FINAL  
+        */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual o número de sua agência?");
+        String agencia = scanner.next();
+        System.out.println("Qual o número da sua conta?");
+        int conta = scanner.nextInt();
+        System.out.println("Qual o valor do seu saldo?");
+        double saldo = scanner.nextDouble();
+        System.out.println("Qual o seu nome?");
+        String nome = scanner.next();
+        System.out.pintf("Olá %d, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo %d já está disponível para saque", nome, agencia, conta, saldo);
+
+        scanner.close();
+
     }
 }
