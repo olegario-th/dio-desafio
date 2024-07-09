92495398d89802dbdef53825303fef082bcb4429
olegario-th <tholegarios@gmail.com>
2024-07-09 00:19:55 -0300

    correção das pontuações e do método de impressão

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -9,15 +9,15 @@ public class ContaTerminal {
        - EXIBIR A MENSAGEM FINAL  
         */
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Qual o número de sua agência?");
+        System.out.println("Qual o numero de sua agencia?");
         String agencia = scanner.next();
-        System.out.println("Qual o número da sua conta?");
+        System.out.println("Qual o numero da sua conta?");
         int conta = scanner.nextInt();
         System.out.println("Qual o valor do seu saldo?");
         double saldo = scanner.nextDouble();
         System.out.println("Qual o seu nome?");
         String nome = scanner.next();
-        System.out.pintf("Olá %d, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo %d já está disponível para saque", nome, agencia, conta, saldo);
+        System.out.println("Ola " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
 
         scanner.close();
 
